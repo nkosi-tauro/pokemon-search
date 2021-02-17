e8c794aa915038da4d48d9c280e4e4dc191ae8d0
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-17 09:29:26 +0200

    added particle backgrounds

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="p-14" >
-    <router-link class="flex justify-center text-4xl text-yellow-700" to="/">Pokemon Search</router-link>
+  <div>
+    <particles-bg type="fountain" :bg="true" />
+    <div class="p-14">
+      <router-link class="flex justify-center text-4xl text-yellow-700" to="/">
+      Pokemon Search
+      </router-link>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
+<script>
+import { ParticlesBg } from "particles-bg-vue";
+export default {
+  name: "App",
+  components: {
+    ParticlesBg,
+  }
+};
+</script>
 <style>
 </style>
